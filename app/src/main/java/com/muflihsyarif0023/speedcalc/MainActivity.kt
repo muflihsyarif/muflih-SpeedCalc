70ce73b3fe4002577f7df4b388a40841d02aa1d6
muflih <muflihsyarif44@gmail.com>
2025-05-04 20:00:43 +0700

    Menambahkan Navigation component

--- app/src/main/java/com/muflihsyarif0023/speedcalc/MainActivity.kt
+++ app/src/main/java/com/muflihsyarif0023/speedcalc/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.muflihsyarif0023.speedcalc.ui.screen.MainScreen
+import com.muflihsyarif0023.speedcalc.navigation.SetupNavGraph
 import com.muflihsyarif0023.speedcalc.ui.theme.SpeedCalcTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SpeedCalcTheme {
-               MainScreen()
+                SetupNavGraph()
             }
         }
     }
---
+++ app/src/main/java/com/muflihsyarif0023/speedcalc/navigation/NavGraph.kt
@@ -0,0 +1,21 @@
+package com.muflihsyarif0023.speedcalc.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.muflihsyarif0023.speedcalc.ui.screen.MainScreen
+
+@Composable
+fun SetupNavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(route = Screen.Home.route){
+            MainScreen()
+        }
+    }
+}
