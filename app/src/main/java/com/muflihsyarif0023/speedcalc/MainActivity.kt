7847d827e0f3ca11211345a0630a4f00df088ec7
muflih <muflihsyarif44@gmail.com>
2025-05-04 15:28:51 +0700

    Memberi Aksi Pada Tombol Saat Diklik

--- app/src/main/java/com/muflihsyarif0023/speedcalc/MainActivity.kt
+++ app/src/main/java/com/muflihsyarif0023/speedcalc/MainActivity.kt
@@ -18,6 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -62,21 +66,24 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier ){
+    var number by remember { mutableIntStateOf(0) }
+
     Column(
         modifier = modifier.fillMaxSize().padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "0",
+            text = number.toString(),
             style = MaterialTheme.typography.displayLarge
         )
         Button(
-            onClick = {},
+            onClick = {number++},
             modifier = Modifier.fillMaxSize(0.5f).padding(top = 16.dp),
             contentPadding = PaddingValues(16.dp)
         ) {
-            Text(text = stringResource(R.string.count))
+            Text(text = stringResource(R.string.count
+            ))
         }
     }
 }
